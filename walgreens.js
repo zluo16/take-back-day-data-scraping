75fb9402b43717a3724260ecb37751a0d55f4b3f
Mike <zluo@alumni.berklee.edu>
2018-03-13 20:46:12 -0400

    going back to scraping

--- walgreens.js
+++ walgreens.js
@@ -1,18 +1,19 @@
 const cheerio = require('cheerio');
+const casper = require('casperjs');
 const rp = require('request-promise');
 const axios = require('axios');
 const zipcodes = require('zipcodes');
 const zips = require('./data/zips-ny.json');
 
-// const options = {
-//   uri: 'https://www.walgreens.com/storelocator/find.jsp?zipecode=10003',
-//   transform: function(body) {
-//     return cheerio.load(body);
-//   }
-// };
-//
-// rp(options).then((cheers) => {
-//   console.log(cheers);
+const options = {
+  uri: 'https://www.walgreens.com/storelocator/find.jsp',
+  transform: function(body) {
+    return cheerio.load(body);
+  }
+};
+
+// rp(options).then(($) => {
+//   console.log($('div').text());
 // }).catch((err) => {
 //   console.log(err);
 // })
@@ -26,17 +27,19 @@ const zips = require('./data/zips-ny.json');
 //   act: 'fndStore',
 //   view: 'fndStoreJSON'
 // }
-
+//
 const zip = zipcodes.lookup(10003);
 
-axios.post('https://services.walgreens.com/api/stores/search', {
-  apiKey: 'l9ozhlJRs9OoL4hzJnkCNUoHLTzUUg3m',
-  afflId: 'storesapi',
-  lat: `${zip.latitude}`,
-  lng: `${zip.longitude}`,
-  requestType: 'locator',
-  act: 'fndStore',
-  view: 'fndStoreJSON'
-}).then(res => {
-  console.log(res.data);
-}).catch(err => console.error(err))
+console.log(zip);
+//
+// axios.post('https://services-qa.walgreens.com/api/stores/search', {
+//   apiKey: 'l9ozhlJRs9OoL4hzJnkCNUoHLTzUUg3m',
+//   afflId: 'storesapi',
+//   lat: `${zip.latitude}`,
+//   lng: `${zip.longitude}`,
+//   requestType: 'locator',
+//   act: 'fndStore',
+//   view: 'fndStoreJSON'
+// }).then(res => {
+//   console.log(res.data);
+// }).catch(err => console.error(err))
